743d1a1e1c7d433ebefb1d52d541089e92cbac83
vesant <v.gonvcalves2006@gmail.com>
2025-06-03 17:16:46 +0100

    Changed miss written

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 import sqlite3
 
 #import de metodos externos
-from func.sqliteCommands import create_table_sql
+from func.sqliteCommands import create_table
 
 #main
 def main():
-    sql = create_table_sql("users", "id INTEGER PRIMARY KEY")
+    sql = create_table("users", "id INTEGER PRIMARY KEY")
     print(sql)
 
 # main classes
